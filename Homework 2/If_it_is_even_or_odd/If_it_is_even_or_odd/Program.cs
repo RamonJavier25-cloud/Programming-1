a729ca0029c1c3797b05ab3c4dfe7d383f416c9a
Ramon Antonio Javier Belen <jersonjavierbelen25@gmail.com>
2025-10-11 21:38:30 -0400

    Add files via upload

---
+++ Homework 2/If_it_is_even_or_odd/If_it_is_even_or_odd/Program.cs
@@ -0,0 +1,33 @@
+﻿
+    string answer = "";
+
+    while (!(answer.Equals("yes", StringComparison.OrdinalIgnoreCase) || answer.Equals("no", StringComparison.OrdinalIgnoreCase)))
+    {
+        Console.WriteLine("Do you want to start the program? (Yes/No)");
+        answer = Console.ReadLine()!;
+    }
+
+    while (answer.Equals("yes", StringComparison.OrdinalIgnoreCase))
+    {
+        Console.WriteLine("Enter a number:");
+        if (int.TryParse(Console.ReadLine(), out int number))
+        {
+            if (number % 2 == 0)
+                Console.WriteLine($"{number} is even.");
+            else
+                Console.WriteLine($"{number} is odd.");
+        }
+        else
+        {
+            Console.WriteLine("Invalid input. Please enter a valid number.");
+        }
+
+
+        answer = "";
+        while (!(answer.Equals("yes", StringComparison.OrdinalIgnoreCase) || answer.Equals("no", StringComparison.OrdinalIgnoreCase)))
+        {
+            Console.WriteLine("Do you want to check another number? (Yes/No)");
+            answer = Console.ReadLine()!;
+        }
+    }
+    Console.WriteLine("Program ended. Goodbye!");
